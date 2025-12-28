899b259663f8bba66a9c12dd8f151e6a73282dbb
Kala Stolze <kayla831j@gmail.com>
2025-12-28 06:55:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5385065145-af2ca",
+  "appId": "1:66544646796:web:cb7388f95041726fa259a9",
+  "apiKey": "AIzaSyDDJXfqBBWHfvBtg8OOhEfxIrBDQWtZ2HM",
+  "authDomain": "studio-5385065145-af2ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "66544646796"
+};
